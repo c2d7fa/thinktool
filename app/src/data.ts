43229ed22a9ad6a45176ff66ab14c0de02d5ed23
Jonas Hvid <mail@johv.dk>
2019-12-28 13:59:46 +0100

    Implement copy, remove and move

--- app/src/data.ts
+++ app/src/data.ts
@@ -19,3 +19,35 @@ export function setContent(things: Things, thing: number, newContent: string): v
   // TODO: Handle case where there is no such thing.
   things[thing].content = newContent;
 }
+
+// Copying, removing and moving items
+
+// TODO: This interface is pretty messy.
+
+// Represents a thing in its exact location. We store the state, because
+// {parent, index} would be invalidated whenever the state changes.
+type ThingInTree = {state: Things; parent: number; index: number};
+type LocationInTree = {parent: number; index: number};
+
+// TODO: Weird interface. We are passing State twice, but I don't think we want
+// to modify the state that we get from source, right?
+function copy(state: Things, source: ThingInTree, target: LocationInTree): void {
+  state[target.parent].children.splice(target.index, 0, state[source.parent].children[source.index]);
+}
+
+function remove(state: Things, item: ThingInTree): void {
+  state[item.parent].children.splice(item.index, 1);
+}
+
+function move(state: Things, source: ThingInTree, target: LocationInTree): void {
+  copy(state, source, target);
+  if (source.parent === target.parent) {
+    if (target.index <= source.index) {
+      remove(state, {state, parent: source.parent, index: source.index + 1});
+    } else {
+      remove(state, source);
+    }
+  } else {
+    remove(state, source);
+  }
+}
