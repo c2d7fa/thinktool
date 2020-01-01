b712e4f6656e9837c4ccef5c951a3e6779b8fd12
Jonas Hvid <mail@johv.dk>
2020-01-01 01:14:08 +0100

    Fix bug where it was impossible to add more than 10 unique items
    Due to Object.keys(...).sort() using string sorting rather than numeric sorting.

--- app/src/data.ts
+++ app/src/data.ts
@@ -4,19 +4,20 @@ export interface ThingData {
 }
 
 export interface Things {
-  [thing: number]: ThingData;
+  next: number;
+  things: {[thing: number]: ThingData};
 }
 
 export function children(things: Things, thing: number): number[] {
-  return things[thing].children;
+  return things.things[thing].children;
 }
 
 export function content(things: Things, thing: number): string {
-  return things[thing].content;
+  return things.things[thing].content;
 }
 
 export function setContent(things: Things, thing: number, newContent: string): Things {
-  return {...things, [thing]: {...things[thing], content: newContent}};
+  return {...things, things: {...things.things, [thing]: {...things.things[thing], content: newContent}}};
 }
 
 export function hasChildren(things: Things, thing: number): boolean {
@@ -24,16 +25,16 @@ export function hasChildren(things: Things, thing: number): boolean {
 }
 
 export function addChild(things: Things, parent: number, child: number): Things {
-  return {...things, [parent]: {...things[parent], children: [...things[parent].children, child]}};
+  return {...things, things: {...things.things, [parent]: {...things.things[parent], children: [...things.things[parent].children, child]}}};
 }
 
 // Make the given child a child of its previous sibling.
 export function indent(things: Things, parent: number, index: number): Things {
-  const result: Things = {...things, [parent]: {...things[parent], children: [...things[parent].children]}};
-  result[parent].children.splice(index, 1);
+  const result: Things = {...things, things: {...things.things, [parent]: {...things.things[parent], children: [...things.things[parent].children]}}};
+  result.things[parent].children.splice(index, 1);
 
-  const newParent = things[parent].children[index - 1];
-  const child = things[parent].children[index];
+  const newParent = things.things[parent].children[index - 1];
+  const child = things.things[parent].children[index];
   return addChild(result, newParent, child);
 }
 
@@ -43,31 +44,29 @@ export function unindent(things: Things, grandparent: number, parentIndex: numbe
 
   // Remove the child from its parent
   const parent = children(things, grandparent)[parentIndex];
-  let result: Things = {...things, [parent]: {...things[parent], children: [...things[parent].children]}};
-  result[parent].children.splice(index, 1);
+  let result: Things = {...things, things: {...things.things, [parent]: {...things.things[parent], children: [...things.things[parent].children]}}};
+  result.things[parent].children.splice(index, 1);
 
   // Make it a child of the grandparent following the parent
-  result = {...result, [grandparent]: {...things[grandparent], children: [...things[grandparent].children]}};
-  result[grandparent].children.splice(parentIndex + 1, 0, thing);
+  result = {...result, things: {...result.things, [grandparent]: {...things.things[grandparent], children: [...things.things[grandparent].children]}}};
+  result.things[grandparent].children.splice(parentIndex + 1, 0, thing);
 
   return result;
 }
 
 export function removeChild(state: Things, parent: number, index: number) {
-  const result = {...state, [parent]: {...state[parent], children: [...state[parent].children]}};
-  result[parent].children.splice(index, 1);
+  const result = {...state, things: {...state.things, [parent]: {...state.things[parent], children: [...state.things[parent].children]}}};
+  result.things[parent].children.splice(index, 1);
   return result;
 }
 
 export function insertChild(state: Things, parent: number, child: number, index: number) {
-  const result = {...state, [parent]: {...state[parent], children: [...state[parent].children]}};
-  result[parent].children.splice(index, 0, child);
+  const result = {...state, things: {...state.things, [parent]: {...state.things[parent], children: [...state.things[parent].children]}}};
+  result.things[parent].children.splice(index, 0, child);
   return result;
 }
 
 export function create(state: Things): [Things, number] {
-  const last = Object.keys(state).sort()[Object.keys(state).length - 1];
-  const next = +last + 1;
-
-  return [{...state, [next]: {content: "", children: []}}, next];
+  const next = state.next + 1;
+  return [{...state, next: next + 1, things: {...state.things, [next]: {content: "", children: []}}}, next];
 }
