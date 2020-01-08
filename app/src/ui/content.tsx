80ed0300063318ae7d1c8591a3bb96a85442dd05
Jonas Hvid <mail@johv.dk>
2020-01-08 22:37:57 +0100

    Remove test code

--- app/src/ui/content.tsx
+++ app/src/ui/content.tsx
@@ -1,20 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-export function Test(props: {}) {
-    const [text, setText] = React.useState("my text")
-    return (
-        <>
-            <PlainText text={text} setText={setText}/>
-            <PlainText text={text} setText={setText}/>
-            <PlainText text={text} setText={setText}/>
-            <PlainText text={text} setText={setText}/>
-            <PlainText text={text} setText={setText}/>
-            <PlainText text={text} setText={setText}/>
-        </>
-    )
-}
-
 export const PlainText = React.forwardRef((props: {text: string, setText(text: string): void, className?: string, onFocus?(ev: React.FocusEvent<HTMLElement>): void, onKeyDown?(ev: React.KeyboardEvent<HTMLElement>): void}, ref?: React.MutableRefObject<HTMLElement>) => {
     if (ref === undefined || ref === null)
         ref = React.useRef()
