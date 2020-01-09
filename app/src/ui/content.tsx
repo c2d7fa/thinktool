6cfa5e6f51e4c463df339c65e211b822ad4a21e4
Jonas Hvid <mail@johv.dk>
2020-01-09 18:39:14 +0100

    Make arrow keys scroll through text before chaning items
    This is implemented in the most hacky way possible, but I'm not sure how to do
    any better right now.
    
    Note that the selection position is not reset when navigating between items.

--- app/src/ui/content.tsx
+++ app/src/ui/content.tsx
@@ -1,5 +1,73 @@
 import * as React from "react";
-import {Editor, EditorState, ContentState, getDefaultKeyBinding} from "draft-js";
+import {Editor, EditorState, ContentState, getDefaultKeyBinding, getVisibleSelectionRect} from "draft-js";
+
+// TODO: The entire implementation of this is an absolute hack. The problem is
+// that we want to handle ArrowUp and ArrowDown events differently depending on
+// whether the user's selection is on the first or last line of the item. But it
+// isn't enough to look at the text content, since an item without any newlines
+// may be wrapped visually.
+
+function firstBlockSelected(editorState: EditorState): boolean {
+  return editorState.getCurrentContent().getKeyBefore(editorState.getSelection().getFocusKey()) == null;
+}
+
+function lastBlockSelected(editorState: EditorState): boolean {
+  return editorState.getCurrentContent().getKeyAfter(editorState.getSelection().getFocusKey()) == null;
+}
+
+function currentSelectionIsOnFirstLineOfSelectedElement(): boolean {
+  const range = window.getSelection().getRangeAt(0);
+  const rect = getVisibleSelectionRect(window);
+
+  const testRange = document.createRange();
+  testRange.setStart(range.startContainer, 0);
+  testRange.collapse();
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(testRange);
+
+  const testRect = getVisibleSelectionRect(window);
+
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(range);
+
+  return rect.top === testRect.top;
+}
+
+function currentSelectionIsOnLastLineOfSelectedElement(): boolean {
+  const range = window.getSelection().getRangeAt(0);
+  const rect = getVisibleSelectionRect(window);
+
+  const testRange = document.createRange();
+  testRange.setStart(range.startContainer, (range.startContainer as Text).length);
+  testRange.collapse();
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(testRange);
+
+  const testRect = getVisibleSelectionRect(window);
+
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(range);
+
+  return rect.top === testRect.top;
+}
+
+function firstLineInBlockSelected(editorState: EditorState): boolean {
+  const block = editorState.getCurrentContent().getBlockForKey(editorState.getSelection().getFocusKey());
+  return (
+    (block.getText().indexOf("\n") === -1 ||  // Block is one line
+      block.getText().indexOf("\n") >= editorState.getSelection().getFocusOffset()) && // Focus is before first newline
+    currentSelectionIsOnFirstLineOfSelectedElement()
+  );
+}
+
+function lastLineInBlockSelected(editorState: EditorState): boolean {
+  const block = editorState.getCurrentContent().getBlockForKey(editorState.getSelection().getFocusKey());
+  return (
+    (block.getText().lastIndexOf("\n") === -1 || // Block is one line
+      block.getText().lastIndexOf("\n") <= editorState.getSelection().getFocusOffset()) && // Focus is after last newline
+    currentSelectionIsOnLastLineOfSelectedElement()
+  );
+}
 
 export const PlainText = React.forwardRef(function PlainText(props: {text: string; setText(text: string): void; className?: string; onFocus?(ev: React.FocusEvent<{}>): void; onKeyDown?(ev: React.KeyboardEvent<{}>): boolean}, ref?: React.RefObject<{focus(): void}>) {
   const ref_ = React.useRef();
@@ -22,17 +90,27 @@ export const PlainText = React.forwardRef(function PlainText(props: {text: strin
   }
 
   function keyBindingFn(ev: React.KeyboardEvent<{}>) {
-    if (props.onKeyDown(ev))
-      return null;
-    else
+    if (ev.key === "ArrowUp" && !(ev.ctrlKey || ev.altKey) && !(firstBlockSelected(editorState) && firstLineInBlockSelected(editorState))) {
+      // Arrow up without modifiers inside text. Use normal action.
       return getDefaultKeyBinding(ev);
+    }
+    if (ev.key === "ArrowDown" && !(ev.ctrlKey || ev.altKey) && !(lastBlockSelected(editorState) && lastLineInBlockSelected(editorState))) {
+      // Arrow down without modifiers inside text. Use normal action.
+      return getDefaultKeyBinding(ev);
+    }
+
+    if (props.onKeyDown(ev)) {
+      return null;
+    } else {
+      return getDefaultKeyBinding(ev);
+    }
   }
 
   return <span className={`content-editable-plain-text ${props.className}`}>
     <Editor
       ref={ref as React.RefObject<Editor>}
       editorState={editorState}
-      onChange={onChange} 
+      onChange={onChange}
       stripPastedStyles={true}
       keyBindingFn={keyBindingFn}
       onFocus={props.onFocus}/>
