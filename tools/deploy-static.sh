75f96b22a537b783a7fa53d2fc95f8a23cb1df96
Jonas Hvid <mail@johv.dk>
2020-03-18 18:00:26 +0100

    Add Cloudflare cache purging step to GitHub Actions workflow

--- tools/deploy-static.sh
+++ tools/deploy-static.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-# Deploy to Azure Storage. Expects AZURE_STORAGE_ACCOUNT and AZURE_STORAGE_KEY
+# Deploy to Azure Storage and purge Cloudflare cache. Expects
+# AZURE_STORAGE_ACCOUNT, AZURE_STORAGE_KEY, CLOUDFLARE_TOKEN and CLOUDFLARE_ZONE
 # to be set.
 
 az storage blob upload-batch -s dist/static -d '$web' -o table
+
+curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE/purge_cache" -H "Authorization: Bearer $CLOUDFLARE_TOKEN" -H 'Content-Type: application/json' --data '{"purge_everything": true}'
