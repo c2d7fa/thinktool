29ffb3be542c5ae1c0f143269cdf04cfeb8006ec
Jonas Hvid <mail@johv.dk>
2020-05-10 18:38:52 +0200

    Use Babel to compile shared client code
    Now we just need TSC for updating declaration files.
    
    We had to introduce some hacks around how configuration files are stored.
    
    We also had to reintroduce that import line for some reason...

--- src/web/main.tsx
+++ src/web/main.tsx
@@ -1,4 +1,10 @@
+import "regenerator-runtime/runtime"; // [TODO] Why do we need this??
+
 import * as Client from "thinktool-client";
+
+import * as Configuration from "../../conf/client.json";
+
+Client.initialize({apiHost: Configuration.apiHost});
 
 (window as any).thinktoolApp = Client.thinktoolApp;
 (window as any).thinktoolDemo = Client.thinktoolDemo;
