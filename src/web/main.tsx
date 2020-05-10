c90e9bfbede0692236f5ee401bb1b5c246ffab2b
Jonas Hvid <mail@johv.dk>
2020-05-10 21:25:31 +0200

    Store demo data on web client

--- src/web/main.tsx
+++ src/web/main.tsx
@@ -1,7 +1,13 @@
 import * as Client from "thinktool-client";
 
 import * as Configuration from "../../conf/client.json";
+import * as DemoData from "./demo-data.json";
+import {Communication} from "../shared/dist";
 
 (window as any).thinktoolApp = () => Client.thinktoolApp({apiHost: Configuration.apiHost});
-(window as any).thinktoolDemo = () => Client.thinktoolDemo({apiHost: Configuration.apiHost});
+(window as any).thinktoolDemo = () =>
+  Client.thinktoolDemo({
+    apiHost: Configuration.apiHost,
+    data: DemoData as Communication.FullStateResponse,
+  });
 (window as any).thinktoolUser = () => Client.thinktoolUser({apiHost: Configuration.apiHost});
