ef406d2fd3fa45e24014a2001fb9c62947d26ce1
Jonas Hvid <mail@johv.dk>
2020-05-16 19:25:50 +0200

    Finish script to send newsletter

--- tools/newsletter/send.py
+++ tools/newsletter/send.py
@@ -6,50 +6,82 @@ import string
 
 import psycopg2
 
-postgres_conn = psycopg2.connect(
-    host=os.getenv("DIAFORM_POSTGRES_HOST"),
-    port=int(os.getenv("DIAFORM_POSTGRES_PORT")),
-    user=os.getenv("DIAFORM_POSTGRES_USERNAME"),
-    password=os.getenv("DIAFORM_POSTGRES_PASSWORD"),
-    dbname="postgres"
-)
-postgres = postgres_conn.cursor()
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-def send_email(email, text, html):
-  with open("email-{}.txt".format(email), "w") as file:
-    file.write(text)
-  with open("email-{}.html".format(email), "w") as file:
-    file.write(html)
+smtp_host = os.getenv("DIAFORM_SMTP_HOST")
+smtp_port = int(os.getenv("DIAFORM_SMTP_PORT"))
+smtp_username = os.getenv("DIAFORM_SMTP_USERNAME")
+smtp_password = os.getenv("DIAFORM_SMTP_PASSWORD")
 
-def send_newsletter(email, unsubscribe):
-  text_template = None
-  with open("newsletter.txt") as file:
-    text_template = file.read()
-  text_output = text_template.format(unsubscribe=unsubscribe)
+subject = "Working on an offline app for Thinktool"
 
-  html_template = None
-  with open("newsletter.html") as file:
-    html_template = file.read()
-  html_output = html_template.format(unsubscribe=unsubscribe)
+print("Connecting over SMTP to {}:{}...".format(smtp_host, smtp_port))
+with smtplib.SMTP_SSL(host=smtp_host, port=smtp_port) as smtp_conn:
+  smtp_conn.ehlo_or_helo_if_needed()
+  smtp_conn.login(smtp_username, smtp_password)
+  print("  Logged in to SMTP server as {}.".format(smtp_username))
 
-  send_email(email, text_output, html_output)  
+  postgres_conn = psycopg2.connect(
+      host=os.getenv("DIAFORM_POSTGRES_HOST"),
+      port=int(os.getenv("DIAFORM_POSTGRES_PORT")),
+      user=os.getenv("DIAFORM_POSTGRES_USERNAME"),
+      password=os.getenv("DIAFORM_POSTGRES_PASSWORD"),
+      dbname="postgres"
+  )
+  postgres = postgres_conn.cursor()
 
-print("\x1B[1mPress ENTER to send...\x1B[0m")
+  def send_email(email, text, html, unsubscribe_post_url):
+    message = MIMEMultipart("alternative") # MIME type is multipart/alternative
+    message["Subject"] = subject
+    message["From"] = "newsletter@thinktool.io"
+    message["To"] = email
+    message["List-Unsubscribe"] = "<{}>".format(unsubscribe_post_url)
+    message["List-Unsubscribe-Post"] = "List-Unsubscribe=One-Click"
+    text_part = MIMEText(text, "plain")
+    html_part = MIMEText(html, "html")
+    message.attach(text_part)
+    message.attach(html_part)
+    smtp_conn.send_message(message)
 
-postgres.execute("SELECT email, registered, unsubscribe_token FROM newsletter_subscriptions ORDER BY registered DESC NULLS FIRST")
-for subscription in postgres.fetchall():
-  email = subscription[0]
-  registered = subscription[1]
-  unsubscribe = subscription[2]
+  def send_newsletter(email, unsubscribe):
+    text_template = None
+    with open("newsletter.txt") as file:
+      text_template = file.read()
+    text_output = text_template.format(unsubscribe=unsubscribe)
 
-  # Ask for confirmation
-  print("  \x1B[30mWAIT\x1B[0m \x1B[32;1m{}\x1B[0m (registered \x1B[34m{:%Y-%m-%d}\x1B[0m): ".format(email, registered), end="")
-  input()
+    html_template = None
+    with open("newsletter.html") as file:
+      html_template = file.read()
+    html_output = html_template.format(unsubscribe=unsubscribe)
 
-  # Send
-  print("\x1B[F  \x1B[1mSENT\x1B[0m\x1B[E", end="")
-  send_newsletter(email, unsubscribe)
+    send_email(email, text_output, html_output, "https://api.thinktool.io/unsubscribe?key={unsubscribe}".format(unsubscribe=unsubscribe))
+
+  print("\x1B[1mPress ENTER to send...\x1B[0m")
+
+  postgres.execute("SELECT email, registered, unsubscribe_token FROM newsletter_subscriptions ORDER BY registered ASC NULLS FIRST")
+  for subscription in postgres.fetchall():
+    email = subscription[0]
+    registered = subscription[1]
+    unsubscribe = subscription[2]
+
+    # Ask for confirmation
+    print("  \x1B[30mWAIT\x1B[0m \x1B[32;1m{}\x1B[0m (registered \x1B[34m{:%Y-%m-%d}\x1B[0m): ".format(email, registered), end="")
+    try:
+      input()
+    except:
+      print("\x1B[G  \x1B[31;1mABRT\x1B[0m\x1B[E", end="")
+      print()
+      exit()
+
+    # Send
+    print("\x1B[F  \x1B[1mSENT\x1B[0m\x1B[E", end="")
+    send_newsletter(email, unsubscribe)
+
+    # Update sent status
+    postgres.execute("UPDATE newsletter_subscriptions SET last_sent = NOW() WHERE email = %s", (email,))
+    postgres_conn.commit()
 
 postgres.close()
-#postgres_conn.commit()
 postgres_conn.close()
