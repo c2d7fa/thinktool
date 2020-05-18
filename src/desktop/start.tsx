df38b1f0a3dfce10f4f655c2335fb10038d75850
Jonas Hvid <mail@johv.dk>
2020-05-18 21:50:53 +0200

    Use electron-builder to bundle Electorn app
    We need to bundle the desktop app for both macOS and Windows.
    
    I tried electron-builder, and it seems like we can get it to
    work, so I went with this option. I didn't carefully explore
    other alternatives.

--- src/desktop/start.tsx
+++ src/desktop/start.tsx
@@ -2,6 +2,8 @@ import * as Client from "thinktool-client";
 import * as Electron from "electron";
 
 console.log("Starting Thinktool desktop application...");
+console.log(Client.thinktoolApp);
+console.log("Running at path:", Electron.app.getAppPath());
 
 // Get rid of warning about the default value of allowRenderProcessReuse being
 // deprecated. We don't care about its value.
@@ -10,7 +12,8 @@ Electron.app.allowRendererProcessReuse = true;
 Electron.app.whenReady().then(() => {
   const window = new Electron.BrowserWindow({});
 
-  window.loadFile("index.html");
+  // [TODO] We need to do build/whatever only when using electron-builder for
+  // some reason. Idk, maybe we should just add a hack to detect when we're
+  // being run inside builder, and then use this then?
+  window.loadFile("build/index.html");
 });
-
-console.log(Client.thinktoolApp);
