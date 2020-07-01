6dccd5353a92b28ced94eb774f835f9e93ec3087
Jonas Hvid <mail@johv.dk>
2020-07-02 01:07:15 +0200

    Implement opening of external links in desktop client
    This method is definitely quite hacky, but I think it works in most
    cases that actually occur in the app.

--- src/desktop/app.tsx
+++ src/desktop/app.tsx
@@ -4,7 +4,24 @@ import * as Client from "thinktool-client";
 const React = Client.React; // If we try to use our own React, we get an error when building.
 
 function ExternalLink(props: {href: string; children: React.ReactNode; [k: string]: any}) {
-  return <span style={{color: "#ccc"}}>{props.href}</span>;
+  const attrs: object = {...props};
+  delete (attrs as any).href;
+  delete (attrs as any).children;
+
+  // Links are relative to https://thinktool.io/ unless otherwise specified.
+  const actualHref = props.href[0] === "/" ? "https://thinktool.io" + props.href : props.href;
+
+  return (
+    <a
+      href="#"
+      onClick={() => {
+        console.log("Opening %o in external", actualHref);
+        Electron.shell.openExternal(actualHref);
+      }}
+      {...attrs}>
+      {props.children}
+    </a>
+  );
 }
 
 const storage = Electron.remote.getGlobal("storage") as Client.Storage.Storage;
