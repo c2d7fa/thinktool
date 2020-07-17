24541ae6a723062891c22ba9858cb5127048714c
Jonas Hvid <mail@johv.dk>
2020-07-18 00:30:58 +0200

    Add real content to splash screen
    We also added some placeholder "images", since we'll want those to be
    the focus of this popup once we have something to put there.

--- src/client/src/ui/Splash.tsx
+++ src/client/src/ui/Splash.tsx
@@ -5,20 +5,20 @@ import * as React from "react";
 // features and how to get started.
 
 export default function Splash(props: {splashCompleted(): void}) {
-  const pages = [page1, page2];
+  const pages = [page1, page2, page3];
   const [currentPageIndex, setCurrentPageIndex] = React.useState<number>(0);
-  const currentPage = pages[currentPageIndex]
+  const currentPage = pages[currentPageIndex];
 
-  const hasPrevious = currentPageIndex !== 0
-  const isLast = currentPageIndex === pages.length - 1
+  const isFirst = currentPageIndex === 0;
+  const isLast = currentPageIndex === pages.length - 1;
 
   function goToPrevious() {
-    setCurrentPageIndex(currentPageIndex - 1)
+    setCurrentPageIndex(currentPageIndex - 1);
   }
 
   function goToNext() {
-    if (isLast) return props.splashCompleted()
-    setCurrentPageIndex(currentPageIndex + 1)
+    if (isLast) return props.splashCompleted();
+    setCurrentPageIndex(currentPageIndex + 1);
   }
 
   return (
@@ -26,8 +26,12 @@ export default function Splash(props: {splashCompleted(): void}) {
       <div className="splash-main">
         {currentPage}
         <div className="splash-nav">
-          <button onClick={() => goToPrevious()} disabled={!hasPrevious}>Back</button>
-          <button className="suggested-button" onClick={() => goToNext()}>{isLast ? "Continue" : "Next"}</button>
+          <button onClick={() => goToPrevious()} disabled={isFirst}>
+            Back
+          </button>
+          <button className="suggested-button" onClick={() => goToNext()}>
+            {isLast ? "Start Tutorial" : isFirst ? "Learn More" : "Continue"}
+          </button>
         </div>
       </div>
     </div>
@@ -36,14 +40,30 @@ export default function Splash(props: {splashCompleted(): void}) {
 
 const page1 = (
   <>
-    <h1>Page 1</h1>
-    <p>This is the first page of this so-called splash screen.</p>
+    <div className="placeholder-image"></div>
+    <h1>Welcome to Thinktool</h1>
+    <p>With multiple parents and bidirectional linking, you'll waste less time on organizing your notes.</p>
   </>
 );
 
 const page2 = (
   <>
-    <h1>Page 2</h1>
-    <p>This is the second page of this so-called splash screen.</p>
+    <div className="placeholder-image"></div>
+    <h1>Multiple parents</h1>
+    <p>
+      Items can have multiple parents, so you don't have to choose one place for them. Thinktool keeps
+      everything in sync.
+    </p>
+  </>
+);
+
+const page3 = (
+  <>
+    <div className="placeholder-image"></div>
+    <h1>Bidirectional links</h1>
+    <p>
+      Associate related items with links. You don't have to keep track of where an item is linked – Thinktool
+      does that for you.
+    </p>
   </>
 );
