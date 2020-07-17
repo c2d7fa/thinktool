df2149544e8faf3aa96fe54236ea13bc748a5ff8
Jonas Hvid <mail@johv.dk>
2020-07-17 23:51:03 +0200

    Support multiple pages in splash screen

--- src/client/src/ui/Splash.tsx
+++ src/client/src/ui/Splash.tsx
@@ -5,12 +5,45 @@ import * as React from "react";
 // features and how to get started.
 
 export default function Splash(props: {splashCompleted(): void}) {
+  const pages = [page1, page2];
+  const [currentPageIndex, setCurrentPageIndex] = React.useState<number>(0);
+  const currentPage = pages[currentPageIndex]
+
+  const hasPrevious = currentPageIndex !== 0
+  const isLast = currentPageIndex === pages.length - 1
+
+  function goToPrevious() {
+    setCurrentPageIndex(currentPageIndex - 1)
+  }
+
+  function goToNext() {
+    if (isLast) return props.splashCompleted()
+    setCurrentPageIndex(currentPageIndex + 1)
+  }
+
   return (
     <div className="splash-outer">
       <div className="splash-main">
-        <div>Hello, this is the so-called splash screen.</div>
-        <button onClick={() => props.splashCompleted()}>Very cool</button>
+        {currentPage}
+        <div className="splash-nav">
+          <button onClick={() => goToPrevious()} disabled={!hasPrevious}>Back</button>
+          <button className="suggested-button" onClick={() => goToNext()}>{isLast ? "Continue" : "Next"}</button>
+        </div>
       </div>
     </div>
   );
 }
+
+const page1 = (
+  <>
+    <h1>Page 1</h1>
+    <p>This is the first page of this so-called splash screen.</p>
+  </>
+);
+
+const page2 = (
+  <>
+    <h1>Page 2</h1>
+    <p>This is the second page of this so-called splash screen.</p>
+  </>
+);
