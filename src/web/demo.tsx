51e0017eb03a9214c5c24ce01aba4c0859d58b40
Jonas Hvid <mail@johv.dk>
2020-09-22 02:45:04 +0200

    Use 'process.env' provided by Parcel for API URL

--- src/web/demo.tsx
+++ src/web/demo.tsx
@@ -2,11 +2,13 @@ import "core-js/stable";
 
 import * as Client from "@thinktool/client";
 
-import * as Configuration from "../../conf/client.json";
+// Provided by Parcel
+declare let process: {env: {[k: string]: string | undefined}};
+
 import * as DemoData from "./demo-data.json";
 import {Communication} from "@thinktool/shared";
 
 Client.thinktoolDemo({
-  apiHost: Configuration.apiHost,
+  apiHost: process.env.DIAFORM_API_HOST!,
   data: DemoData as Communication.FullStateResponse,
 });
