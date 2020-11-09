8b5b7cf21db4b315c69fcf711952a862f57f453a
Jonas Hvid <mail@johv.dk>
2020-11-09 18:34:06 +0100

    Move HTML and other web assets to web package

--- tools/build-web.sh
+++ tools/build-web.sh
@@ -27,25 +27,6 @@ cp src/static/sitemap.txt dist/static
 echo "Building robots.txt..."
 cp src/static/robots.txt dist/staticp
 
-echo "Building HTML templates..."
-node <<"EOF"
-const h = require("handlebars")
-const fs = require("fs")
-
-const options = {apiUrl: process.env.DIAFORM_API_HOST}
-
-for (const file of fs.readdirSync("src/markup")) {
-  const template = fs.readFileSync(`src/markup/${file}`, "utf8")
-  const html = h.compile(template)(options)
-
-  const name = file.replace(/\..*$/, "")
-  const outPath = name === "landing" ? `dist/static/index.html` : `dist/static/${name}.html`
-
-  console.log(`  Writing ${outPath}`)
-  fs.writeFileSync(outPath, html)
-}
-EOF
-
 echo "Building blog..."
 ./tools/build-blog.sh
 
