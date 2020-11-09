39c8821d81add4749ed71a8d8967809e997c49ec
Jonas Hvid <mail@johv.dk>
2020-11-09 19:32:49 +0100

    Update desktop package for new build structure

--- src/desktop/build-static-and-client-for-desktop.sh
+++ src/desktop/build-static-and-client-for-desktop.sh
@@ -1,24 +1,20 @@
 #!/usr/bin/env bash
-
-set -e
+set -eux -o pipefail
 
 mkdir -p build
 
 cd ../..
 
-echo "Installing dependencies..."
-npm ci
-
 echo "Buliding images..."
 cp src/static/*.svg src/desktop/build
 cp src/static/*.png src/desktop/build
-
-echo "Building stylesheets..."
-cp node_modules/@thinktool/client/dist/app.css src/desktop/build
 
 cd src/desktop
 
 echo "Building static resources specific to desktop client..."
 cp -r static/* build
+
+echo "Building stylesheets..."
+cp node_modules/@thinktool/client/dist/app.css build
 
 echo "Building JavaScript..."
