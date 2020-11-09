b7fc138d7d367b3f5935bf21b88d5bdde08b4ed5
Jonas Hvid <mail@johv.dk>
2020-11-09 20:48:44 +0100

    Build demo with Next.js instead of Parcel
    This *mostly* works pretty well. There are only two things that are slightly
    inelegant about this:
    
    1) We need to use resources from outside this package (namely 'app.css' in
    '@thinktool/client' and the shared resouce 'icon.png'). We just manually build
    these files into the 'public/' directory. I can't think of a better approach
    here.
    
    2) Parcel won't let us remove 'demo.html' since we have a link to it, so we just
    added a placeholder instead. This is a hack, but it's only there until we're
    done with Parcel, and the build still works.

--- src/web/demo.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import "core-js/stable";
-
-import * as Thinktool from "@thinktool/client";
-import * as DemoData from "./demo-data.json";
-
-ReactDOM.render(<Thinktool.Demo data={DemoData} />, document.getElementById("root"));
---
+++ src/web/next/next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  async rewrites() {
+    return [
+      {source: "/:path*.html", "destination": "/:path*/"},
+    ]
+  },
+}
