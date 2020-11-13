afa30324f8a5109cb8d6378ba764c391caf1f1c3
Jonas Hvid <mail@johv.dk>
2020-11-13 22:01:42 +0100

    Build user settings page with Next.js

---
+++ src/web/next/pages/user.tsx
@@ -0,0 +1,29 @@
+import Head from "next/head";
+
+import * as React from "react";
+import * as Thinktool from "@thinktool/client";
+
+import "@thinktool/client/dist/app.css";
+
+export async function getStaticProps() {
+  return {props: {apiHost: process.env.DIAFORM_API_HOST}};
+}
+
+export default function App(props: {apiHost: string}) {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>Thinktool</title>
+        <link rel="icon" href="/icon.png" type="image/png" />
+        <link rel="prefetch" href="https://thinktool.io/splash-welcome.png" />
+        <link rel="prefetch" href="https://thinktool.io/splash-bidirectional-links.svg" />
+        <link rel="prefetch" href="https://thinktool.io/splash-multiple-parents.svg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script src="https://kit.fontawesome.com/d7c222beb5.js" crossOrigin="anonymous" />
+      </Head>
+      {process.browser && <Thinktool.User apiHost={props.apiHost} />}
+      <script data-goatcounter="https://thinktool.goatcounter.com/count" async src="//gc.zgo.at/count.js" />
+    </>
+  );
+}
--- src/web/user.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import "core-js/stable";
-import * as Thinktool from "@thinktool/client";
-
-// Provided by Parcel
-declare let process: {env: {[k: string]: string | undefined}};
-
-ReactDOM.render(<Thinktool.User apiHost={process.env.DIAFORM_API_HOST!} />, document.getElementById("user"));
