826aa31b9a65b9cce2e8516f26d3ca4f279ade1e
Jonas Hvid <mail@johv.dk>
2020-12-14 21:58:58 +0100

    Update desktop app to latest Electron version
    This required us to make some changes, since 'remote' is apparetly no longer
    something you should use. We now register an IPC callback in the main process,
    and then just use that in the render process. The renderer now does most of the
    work.

--- src/desktop/start.tsx
+++ src/desktop/start.tsx
@@ -1,12 +1,5 @@
 import * as Electron from "electron";
 
-import * as Client from "@thinktool/client";
-import * as SqliteStorage from "./sqlite-storage";
-
-// Get rid of warning about the default value of allowRenderProcessReuse being
-// deprecated. We don't care about its value.
-Electron.app.allowRendererProcessReuse = true;
-
 Electron.app.whenReady().then(async () => {
   const window = new Electron.BrowserWindow({
     webPreferences: {
@@ -16,16 +9,12 @@ Electron.app.whenReady().then(async () => {
 
   window.setMenu(null);
 
-  const path = Electron.dialog.showSaveDialogSync(window, {
-    title: "Open or Create File",
-    buttonLabel: "Open",
+  Electron.ipcMain.handle("open-file", () => {
+    return Electron.dialog.showSaveDialogSync(window, {
+      title: "Open or Create File",
+      buttonLabel: "Open",
+    });
   });
-
-  if (path === undefined) {
-    (global as any).storage = Client.Storage.ignore();
-  } else {
-    (global as any).storage = await SqliteStorage.initialize(path);
-  }
 
   // [TODO] We need to do build/whatever only when using electron-builder for
   // some reason. Idk, maybe we should just add a hack to detect when we're
