d25ce5d7c7cf5f8f74a5e2d73e8aecf7fc734ffd
Jonas Hvid <mail@johv.dk>
2021-04-17 16:58:31 +0200

    Extract top bar to separate module

---
+++ src/client/src/ui/TopBar.tsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import {Context} from "../context";
+import {ExternalLink} from "./ExternalLink";
+
+export function useTopBarProps(
+  context: Context,
+  args: {isToolbarShown: boolean; setIsToolbarShown(b: boolean): void; username?: string},
+): Parameters<typeof TopBar>[0] {
+  return {
+    isToolbarShown: args.isToolbarShown,
+    login:
+      args.username === undefined || context.server === undefined
+        ? null
+        : {
+            username: args.username,
+            logOutUrl: context.server.logOutUrl,
+          },
+    onToggleToolbar: () => args.setIsToolbarShown(!args.isToolbarShown),
+  };
+}
+
+export function TopBar(props: {
+  isToolbarShown: boolean;
+  login: null | {username: string; logOutUrl: string};
+
+  onToggleToolbar(): void;
+}) {
+  return (
+    <div className="top-bar">
+      <ExternalLink className="logo" href="/">
+        Thinktool
+      </ExternalLink>
+      <button onClick={() => props.onToggleToolbar()}>
+        <i className="icon fas fa-bars" />
+        {props.isToolbarShown ? "Hide" : "Show"} Menu
+      </button>
+      {props.login && (
+        <div id="current-user">
+          <ExternalLink className="username" href="/user.html">
+            {props.login.username}
+          </ExternalLink>
+          <a className="log-out" href={props.login.logOutUrl}>
+            Log Out
+          </a>
+        </div>
+      )}
+    </div>
+  );
+}
