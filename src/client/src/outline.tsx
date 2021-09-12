8a98196b685d7172c17bcc35470b16870ccb80d9
Jonas Hvid <mail@johv.dk>
2021-09-12 23:16:11 +0200

    Use modular class names for part of outline module

--- src/client/src/outline.tsx
+++ src/client/src/outline.tsx
@@ -1,13 +1,15 @@
 import * as React from "react";
 
+const style = require("./outline.module.scss").default;
+
 import * as Data from "./data";
 import * as T from "./tree";
 import * as A from "./app";
 import * as Drag from "./drag";
 import * as Item from "./item";
+import * as E from "./editing";
 
 import * as Editor from "./ui/Editor";
-import * as SelectedItem from "./ui/SelectedItem";
 import * as PlaceholderItem from "./ui/PlaceholderItem";
 
 type Outline = {root: Item.ItemData; rootParents: Item.ItemData[]; isRootFolded: boolean};
@@ -51,7 +53,7 @@ export function fromApp(app: A.App): Outline {
   return {
     root: dataItemizeNode(app, T.root(app.tree)),
     rootParents: T.otherParentsChildren(app.tree, T.root(app.tree)).map((n) => dataItemizeNode(app, n)),
-    isRootFolded: SelectedItem.isFolded(app),
+    isRootFolded: isFolded(app),
   };
 }
 
@@ -63,28 +65,56 @@ export const Outline = React.memo(function ({
   onItemEvent(event: Item.ItemEvent): void;
 }) {
   return (
-    <div className="overview">
+    <div className={style.outer}>
       <ParentsOutline parents={outline.rootParents} onItemEvent={onItemEvent} />
-      <div className="overview-main">
-        <SelectedItem.SelectedItem
+      <div className={style.inner}>
+        <SelectedItem
           onEditEvent={(event) => onItemEvent({type: "edit", id: outline.root.id, event})}
           isFolded={outline.isRootFolded}
           unfold={() => onItemEvent({type: "unfold", id: outline.root.id})}
           editor={outline.root.editor}
           hasFocus={outline.root.hasFocus}
         />
-        <div className="children">
+        <div className={style.children}>
           <Item.Subtree parent={outline.root} onItemEvent={onItemEvent} />
         </div>
       </div>
       {outline.root.references.state !== "empty" && (
         <>
-          <div className="references">
-            <h1 className="link-section">References</h1>
+          <div className={style.references}>
+            <h1 className={style.referencesheader}>References</h1>
             <ReferencesOutline references={outline.root.references} onItemEvent={onItemEvent} />
           </div>
         </>
       )}
+    </div>
+  );
+});
+
+function isFolded(app: A.App) {
+  let isFolded = false;
+  for (const child of T.children(app.tree, T.root(app.tree))) {
+    if (!T.expanded(app.tree, child)) {
+      isFolded = true;
+      break;
+    }
+  }
+  return isFolded;
+}
+
+const SelectedItem = React.memo(function SelectedItem(props: {
+  editor: E.Editor;
+  hasFocus: boolean;
+  onEditEvent(event: Editor.Event): void;
+  isFolded: boolean;
+  unfold(): void;
+}) {
+  return (
+    <div className={style.root}>
+      <button className={style.unfold} onClick={props.unfold} disabled={!props.isFolded}>
+        <span className="fas fa-fw fa-stream" />
+      </button>
+      <Editor.Editor editor={props.editor} hasFocus={props.hasFocus} onEvent={props.onEditEvent} />
     </div>
   );
 });
@@ -110,8 +140,8 @@ function ParentsOutline(props: {parents: Item.ItemData[]; onItemEvent: (event: I
   ));
 
   return (
-    <div className="parents">
-      <h1 className="link-section">Parents</h1>
+    <div className={style.parents}>
+      <h1 className={style.referencesheader}>Parents</h1>
       <ul className="subtree">{parentItems}</ul>
     </div>
   );
