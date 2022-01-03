a81a4f87bd9bdb53ce80a7189ea9353ddffcc116
Jonas Hvid <mail@johv.dk>
2022-01-03 04:01:41 +0100

    Add failing test for editor cursor position after moving item

--- src/client/src/app/outline.test.ts
+++ src/client/src/app/outline.test.ts
@@ -40,10 +40,57 @@ describe("in an app with two items", () => {
   });
 
   describe("after focusing the first item", () => {
-    const afterFocus = A.outline(A.update(base, {type: "focus", id: baseOutline.root.children[0].id}));
+    const afterFocus = A.update(base, {type: "focus", id: baseOutline.root.children[0].id});
+    const afterFocusOutline = A.outline(afterFocus);
 
     test("that item becomes focused", () => {
-      expect(afterFocus.root.children).toMatchObject([{hasFocus: true}, {hasFocus: false}]);
+      expect(afterFocusOutline.root.children).toMatchObject([{hasFocus: true}, {hasFocus: false}]);
+    });
+
+    describe("after setting the cursor position", () => {
+      const focusedItem = afterFocusOutline.root.children.find((c) => c.hasFocus)!;
+      const afterCursor = A.update(afterFocus, {
+        type: "item",
+        event: {
+          id: focusedItem.id,
+          type: "edit",
+          event: {tag: "edit", editor: {...focusedItem.editor, selection: {from: 5, to: 5}}, focused: true},
+        },
+      });
+
+      test("the cursor position is reflected in the outline", () => {
+        expect(A.outline(afterCursor).root.children).toMatchObject([
+          {editor: {content: ["Item 1"], selection: {from: 5, to: 5}}},
+          {editor: {content: ["Item 2"]}},
+        ]);
+      });
+
+      describe("after moving the item down", () => {
+        const focusedId = A.outline(afterCursor).root.children.find((c) => c.hasFocus)!.id;
+        const afterDown = A.update(afterCursor, {
+          type: "item",
+          event: {id: focusedId, type: "edit", event: {tag: "action", action: "down"}},
+        });
+
+        test("the positions of the items are swapped", () => {
+          expect(A.outline(afterDown).root.children).toMatchObject([
+            {editor: {content: ["Item 2"]}},
+            {editor: {content: ["Item 1"]}},
+          ]);
+        });
+
+        test("the previously focused item is still focused", () => {
+          expect(A.outline(afterDown).root.children).toMatchObject([{hasFocus: false}, {hasFocus: true}]);
+        });
+
+        // Bug: Cursor position is currently *not* preserved across moves
+        test.skip("the cursor position is still reflected in the outline", () => {
+          expect(A.outline(afterDown).root.children).toMatchObject([
+            {editor: {content: ["Item 2"]}},
+            {editor: {content: ["Item 1"], selection: {from: 5, to: 5}}},
+          ]);
+        });
+      });
     });
   });
 });
