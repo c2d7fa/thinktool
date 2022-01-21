6a5daee4e929d39bb93d20e633f0066186336d98
Jonas Hvid <mail@johv.dk>
2022-01-22 00:28:57 +0100

    Configure Next.js to use trailing slashes for directories
    This fixes a bug where the link to the blox index (`/blog/`) would break in
    production (but not in the development environment for some reason).

--- src/web/next.config.js
+++ src/web/next.config.js
@@ -4,10 +4,5 @@ module.exports = {
       {source: "/:path*.html", "destination": "/:path*/"},
     ]
   },
-  async exportPathMap(defaultPathMap, {dev, dir, outDir, distDir, buildId}) {
-    return {
-      ...defaultPathMap,
-      "/blog/index.html": {page: "/blog"},
-    };
-  },
+  trailingSlash: true,
 };
