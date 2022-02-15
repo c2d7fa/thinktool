542737f4961989db63cac69b6e24c9cf801b2edf
Jonas Hvid <mail@johv.dk>
2022-02-15 15:16:49 +0100

    Add test for using popup to insert sibling item

--- src/client/src/popup/popup.test.ts
+++ src/client/src/popup/popup.test.ts
@@ -48,3 +48,72 @@ describe("when opening popup while text is selected", () => {
     });
   });
 });
+
+describe("searching for and selecting an item", () => {
+  const app = A.after(
+    {
+      "0": {content: ["Root item"], children: ["1", "2"]},
+      "1": {content: ["Some item"]},
+      "2": {content: ["Another item"]},
+    },
+    [(view) => ({type: "focus", id: (view as A.Outline).root.children[0].id})],
+  );
+
+  describe("at first", () => {
+    test("the popup is closed", () => {
+      expect(A.view(app).popup).toMatchObject({open: false});
+    });
+
+    test("the root item has two children", () => {
+      expect((A.view(app) as A.Outline).root.children).toHaveLength(2);
+    });
+  });
+
+  const afterOpeningPopup = A.after(app, [{type: "action", action: "insert-sibling"}]);
+
+  describe("after triggering the 'insert sibling' action", () => {
+    test("the popup is shown", () => {
+      expect(A.view(afterOpeningPopup).popup).toMatchObject({open: true});
+    });
+
+    test("the query text is empty", () => {
+      expect(A.view(afterOpeningPopup).popup).toMatchObject({query: ""});
+    });
+  });
+
+  const afterQuery = A.after(afterOpeningPopup, [{topic: "popup", type: "query", query: "Another item"}]);
+
+  describe("after searching for an item", () => {
+    test("the query text is updated", () => {
+      expect(A.view(afterQuery).popup).toMatchObject({query: "Another item"});
+    });
+
+    test("the matching item is the first result", () => {
+      expect(A.view(afterQuery).popup).toMatchObject({results: [{content: ["Another item"], isSelected: true}]});
+    });
+  });
+
+  const afterSelecting = A.after(afterQuery, [{topic: "popup", type: "select"}]);
+
+  describe("after selecting an item", () => {
+    test("the popup is closed", () => {
+      expect(A.view(afterSelecting).popup).toMatchObject({open: false});
+    });
+
+    test("the selected item is inserted as a sibling", () => {
+      expect((A.view(afterSelecting) as A.Outline).root.children.map((ch) => ch.editor.content)).toEqual([
+        ["Some item"],
+        ["Another item"],
+        ["Another item"],
+      ]);
+    });
+
+    test("the newly inserted item has focus", () => {
+      expect((A.view(afterSelecting) as A.Outline).root.children.map((ch) => ch.hasFocus)).toEqual([
+        false,
+        true,
+        false,
+      ]);
+    });
+  });
+});
