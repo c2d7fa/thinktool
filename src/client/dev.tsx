018784d8aa6007d42117756e726bf97103619017
Jonas Hvid <mail@johv.dk>
2022-07-09 21:23:18 +0200

    Implement updating and synchronizing content in fake server

--- src/client/dev.tsx
+++ src/client/dev.tsx
@@ -28,6 +28,10 @@ appElement2.id = "app";
 iframe2.contentDocument?.body.appendChild(appElement2);
 
 const server = (() => {
+  let data = DemoData;
+
+  let subscribers: [string, (changes: string[]) => void][] = [];
+
   let simulatingDisconnected = false;
 
   (window as any).simulateDisconnected = () => {
@@ -39,13 +43,40 @@ const server = (() => {
   };
 
   return {
+    async fetchData() {
+      return data;
+    },
+
     isDown() {
       return simulatingDisconnected;
+    },
+
+    async setContent(clientId: string, thing: string, content: Client.Communication.Content) {
+      if (simulatingDisconnected) return;
+
+      console.log("setContent", clientId, thing, content);
+
+      data.things = data.things.map((t) => {
+        if (t.name === thing) {
+          t.content = content;
+        }
+        return t;
+      });
+
+      for (const [clientId_, callback] of subscribers) {
+        if (clientId_ !== clientId) {
+          callback([thing]);
+        }
+      }
+    },
+
+    async subscribe(clientId: string, callback: (changes: string[]) => void) {
+      subscribers.push([clientId, callback]);
     },
   };
 })();
 
-function fakeServer(): Client.RemoteServer {
+function fakeServer(clientId: string): Client.RemoteServer {
   let handleErrorCallbacks: ((error: Client.RemoteServerError) => void)[] = [];
   let onChangesCallbacks: ((changes: string[]) => void)[] = [];
 
@@ -57,6 +88,10 @@ function fakeServer(): Client.RemoteServer {
     }
   }
 
+  server.subscribe(clientId, (change) => {
+    onChangesCallbacks.forEach((callback) => callback(change));
+  });
+
   return {
     async getFullState() {
       if (server.isDown()) {
@@ -64,11 +99,11 @@ function fakeServer(): Client.RemoteServer {
         throw "unable to get response from server";
       }
 
-      return DemoData;
+      return server.fetchData();
     },
 
     async setContent(thing: string, content: Client.Communication.Content) {
-      console.log("setContent", thing, content);
+      await server.setContent(clientId, thing, content);
     },
 
     async deleteThing(thing: string) {
@@ -104,7 +139,13 @@ function fakeServer(): Client.RemoteServer {
     },
 
     async getThingData(thing: string) {
-      return null;
+      const result = (await server.fetchData()).things.find((t) => t.name === thing);
+      if (!result) return null;
+      return {
+        isPage: false,
+        children: result.children,
+        content: result.content,
+      };
     },
 
     async deleteAccount(account: string) {
@@ -135,8 +176,8 @@ function fakeServer(): Client.RemoteServer {
   };
 }
 
-ReactDOM.render(<Client.App remote={fakeServer()} />, iframe1.contentDocument?.getElementById("app")!);
-ReactDOM.render(<Client.App remote={fakeServer()} />, iframe2.contentDocument?.getElementById("app")!);
+ReactDOM.render(<Client.App remote={fakeServer("1")} />, iframe1.contentDocument?.getElementById("app")!);
+ReactDOM.render(<Client.App remote={fakeServer("2")} />, iframe2.contentDocument?.getElementById("app")!);
 
 for (const el of document.querySelectorAll("style")) {
   iframe1.contentDocument?.head.appendChild(el.cloneNode(true));
